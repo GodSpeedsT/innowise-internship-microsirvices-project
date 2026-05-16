a8df01eb436793122598ebb9875edc6954f3831a
GodSpeedsT <167005914+GodSpeedsT@users.noreply.github.com>
2026-05-16 23:40:00 +0300

    feat:
    Add repositories and specifiacations for them

--- user-service/src/main/java/com/innowise/userservice/repository/PaymentCardRepository.java
+++ user-service/src/main/java/com/innowise/userservice/repository/PaymentCardRepository.java
@@ -1,12 +1,33 @@
 package com.innowise.userservice.repository;
 
 import com.innowise.userservice.entity.PaymentCard;
-import java.util.Optional;
+import org.springframework.data.repository.query.Param;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface PaymentCardRepository extends JpaRepository<PaymentCard, UUID> {
-PaymentCard save(PaymentCard paymentCard);
-Optional<PaymentCard> findById(UUID id);
+@Repository
+public interface PaymentCardRepository extends JpaRepository<PaymentCard, UUID>,
+    JpaSpecificationExecutor<PaymentCard> {
+
+  List<PaymentCard> findByUserId(UUID userId);
+
+  Page<PaymentCard> findByUserId(UUID userId, Pageable pageable);
+
+  @Query(value = "SELECT * FROM payment_cards WHERE holder ILIKE '%' || :holder || '%'", nativeQuery = true)
+  List<PaymentCard> findCardsNative(@Param("holder") String holder);
+
+  @Transactional
+  @Modifying
+  @Query("UPDATE PaymentCard pc SET pc.active = :active WHERE pc.id = :id")
+  void setActiveStatus(@Param("id") UUID id, @Param("active") boolean active);
+
 
 }
