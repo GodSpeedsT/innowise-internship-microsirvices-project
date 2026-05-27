01d6fad360dc4ea1ace92293763996a23e62a6b7
GodSpeedsT <167005914+GodSpeedsT@users.noreply.github.com>
2026-05-28 00:31:48 +0300

    feat: create dto`s

--- auth-service/src/main/java/com/innowise/authservice/dto/AuthResponse.java
+++ auth-service/src/main/java/com/innowise/authservice/dto/AuthResponse.java
@@ -1,7 +1,15 @@
 package com.innowise.authservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthResponse {
-
-
-
+  private String accessToken;
+  private String refreshToken;
 }
--- auth-service/src/main/java/com/innowise/authservice/dto/UserDtoForUserService.java
+++ auth-service/src/main/java/com/innowise/authservice/dto/UserDtoForUserService.java
@@ -1,12 +1,12 @@
 package com.innowise.authservice.dto;
 
-import com.innowise.authservice.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,22 +16,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AuthRequest {
+public class UserDtoForUserService {
 
-  @NotBlank(message = "Login cannot be empty")
-  @Size(min = 2, max = 100, message = "Login must be between 2 and 100 characters")
-  private String login;
-  @NotBlank(message = "Password cannot be empty")
-  @Size(min = 12, max = 100, message = "Password must be at least 12 characters long")
-  private String password;
-  @NotNull(message = "Role must be specified")
-  private Role role;
-
+  @NotNull
+  private UUID id;
   @NotBlank(message = "Name is required")
-  @Size(max = 50, message = "Name must not exceed 50 characters")
+  @Size(max = 50)
   private String name;
   @NotBlank(message = "Surname is required")
-  @Size(max = 50, message = "Surname must not exceed 50 characters")
+  @Size(max = 50)
   private String surname;
   @NotBlank(message = "Email is required")
   @Email(message = "Enter a valid email")
@@ -39,6 +32,4 @@ public class AuthRequest {
   @NotNull(message = "Birth date is required")
   @Past(message = "Birth date must be in the past")
   private LocalDate birthDate;
-
-
 }
