5ab18a7b8f36c46d45bed5dd5e31c543153535ff
GodSpeedsT <167005914+GodSpeedsT@users.noreply.github.com>
2026-05-28 00:30:01 +0300

    feat: create entity ,enable jpa auditing , configure liquibase and service profiles

--- auth-service/src/main/java/com/innowise/authservice/entity/AuthUser.java
+++ auth-service/src/main/java/com/innowise/authservice/entity/AuthUser.java
@@ -2,18 +2,23 @@ package com.innowise.authservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "auth_users")
@@ -22,19 +27,27 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class AuthUser {
 
   @Id
-  @Column(name = "id", nullable = false)
+  @Column(name = "id", nullable = false,updatable = false)
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   @Column(name = "login", nullable = false, unique = true)
   private String login;
-  @Column(name = "email", nullable = false)
-  private String email;
   @Column(name = "password", nullable = false)
   private String password;
-  @Column(name = "role", nullable = false)
+  @Column(name = "email", nullable = false, unique = true)
+  private String email;
   @Enumerated(EnumType.STRING)
+  @Column(name = "role", nullable = false)
   private Role role;
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
 }
